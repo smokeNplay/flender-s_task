383d209c5754d8d85b5ef256c39acf684b53aed1
Il Losev <losev2710@gmail.com>
2020-01-14 13:18:10 +0200

    Add validations

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,8 @@
 class Message < ApplicationRecord
+  validates :email, :first_name, :last_name, :amount, presence: true
+  validates :email, format: {
+    with: URI::MailTo::EMAIL_REGEXP,
+    message: 'Requires a valid email format'
+  }
+  validates :amount, numericality: { only_integer: true }
 end
